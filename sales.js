c05f6645de96e34fc4168f42dcf48f252f482a26
Team Tokko Support <21377149+twinnybloggers@users.noreply.github.com>
2022-03-23 18:29:19 +0700

    Update sales.js

--- sales.js
+++ sales.js
@@ -2,7 +2,7 @@
     "status": 200,
     "info": "Ditemukan",
     "result": {
-        "3499": {
+        "1": {
             "url_checkout": "https:\/\/twinnybloggers.github.io\/information\/whatsappstore.html",
             "payment_file": "https:\/\/cdn.statically.io\/img\/member.kangrian.net\/w=500,f=webp\/img\/upload\/1640375090_61c62332dcfe1.jpg",
             "buyer": "Bima Mhd Ghaluh",
@@ -11,7 +11,7 @@
             "variant": "Lisensi Personal",
             "timestamp": "2021-12-25 12:08:12"
         },
-        "3496": {
+        "2": {
             "url_checkout": "https:\/\/tokkoblogger.blogspot.com\/2022\/03\/twinny-property-template.html",
             "payment_file": "https:\/\/cdn.statically.io\/img\/member.kangrian.net\/w=500,f=webp\/img\/upload\/1640166735_61c2f54fd2e57.jpeg",
             "buyer": "Andi rio",
@@ -20,7 +20,7 @@
             "variant": "Lisensi Standar",
             "timestamp": "2021-12-22 16:56:00"
         },
-        "3497": {
+        "3": {
             "url_checkout": "https:\/\/tokkoblogger.blogspot.com\/2022\/03\/blog-white-store-bws.html",
             "payment_file": "https:\/\/cdn.statically.io\/img\/member.kangrian.net\/w=500,f=webp\/img\/upload\/1640166692_61c2f524447b5.jpeg",
             "buyer": "Agung Pio",
@@ -29,7 +29,7 @@
             "variant": "Lisensi Standar",
             "timestamp": "2021-12-22 16:53:12"
         },
-        "3495": {
+        "4": {
             "url_checkout": "https:\/\/twinnybloggers.github.io\/information\/whatsappstore.html",
             "payment_file": "https:\/\/cdn.statically.io\/img\/member.kangrian.net\/w=500,f=webp\/img\/upload\/1640106809_61c20b39a0a50.jpeg",
             "buyer": "Fadhil Setiono",
@@ -38,7 +38,7 @@
             "variant": "Lisensi Personal",
             "timestamp": "2021-12-22 00:20:41"
         },
-        "3492": {
+        "5": {
             "url_checkout": "https:\/\/twinnybloggers.github.io\/information\/whatsappstore.html",
             "payment_file": "https:\/\/cdn.statically.io\/img\/member.kangrian.net\/w=500,f=webp\/img\/upload\/1639890578_61bebe9288bfc.png",
             "buyer": "Muhamad Faizin",
@@ -46,7 +46,7 @@
             "variant": "Lisensi Personal",
             "timestamp": "2021-12-19 12:12:13"
         },
-        "3489": {
+        "6": {
             "url_checkout": "https:\/\/tokkoblogger.blogspot.com\/2022\/03\/eat-your-green-whatstore.html",
             "payment_file": "https:\/\/cdn.statically.io\/img\/member.kangrian.net\/w=500,f=webp\/img\/upload\/1639636408_61baddb880512.png",
             "buyer": "Rhendrik Rian",
